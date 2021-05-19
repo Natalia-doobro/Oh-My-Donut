c0187c06945ddb8f247840c444015c88e7fbd1e1
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2021-05-19 13:40:07 +0300

    Правки перед отправлением

--- src/js/bt.js
+++ src/js/bt.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-bt-open]'),
+    closeModalBtn: document.querySelector('[data-bt-close]'),
+    modal: document.querySelector('[data-bt]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
--- src/js/button.js
+++ src/js/button.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+      openModalBtn: document.querySelector('[data-button-open]'),
+      closeModalBtn: document.querySelector('[data-button-close]'),
+      modal: document.querySelector('[data-button]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
